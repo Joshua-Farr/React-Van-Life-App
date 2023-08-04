fbb2fbfc8de7c51e9d61d7cd0d2cdb1206d669c4
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-08-04 07:55:25 -0500

    Initial Commit

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NavBar(){
+    return(
+        <div>
+
+
+        <img src="" alt="" />
+        <h1>This is a navbar!!</h1>
+        </div>
+    )
+
+}
