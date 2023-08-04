22a5676924203a9e7b7dee201b0a97e7ae7d2cfc
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-08-04 09:57:07 -0500

    Added About Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
 import MainContent from "./MainContent";
+import About from "./Components/About";
 import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <MainContent />
-      <Footer />
+      <About />
     </BrowserRouter>
   );
 }
